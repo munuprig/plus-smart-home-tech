48f503bd3996f95cfe7d35970eb03007806505fd
munuprig <egorprygun@ya.ru>
2026-05-11 16:22:42 +0700

    21.2.0

--- commerce/warehouse/src/main/java/ru/yandex/practicum/config/QuantityThresholdProperties.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/config/QuantityThresholdProperties.java
@@ -3,9 +3,11 @@ package ru.yandex.practicum.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "warehouse.quantity-thresholds")
 public class QuantityThresholdProperties {
 
